9b80aed8dcbe1b04f62638dd96cd6ca80b3d7248
Shreyank Kiran <shreyank098@gmail.com>
2026-06-30 09:37:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-with open("TinyLlama_Compression_Book (1).html", "r", encoding="utf-8") as f:
+with open("milestone_3d_white.html", "r", encoding="utf-8") as f:
     html_content = f.read()
 
 components.html(html_content, height=800, scrolling=True)
